2cd8233801ebcfab3c910eb26c1eb02e68583ddc
netosilveira <me@netosilveira.dev>
2022-01-21 11:57:03 -0300

    folder

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <Copia />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Copia from "./components/Copia";
+
+export default {
+  name: "App",
+
+  components: {
+    Header,
+    Copia,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app class="teste">
+    <v-toolbar dark color="grey" class="teste">
+      <v-toolbar-title>
+        <img
+          src="https://sherlockjuices.com/img/sherlock.png"
+          alt="Sherlock"
+          height="30px"
+        />
+      </v-toolbar-title>
+      <v-toolbar-item>
+        <v-btn flat> Teste 1 </v-btn>
+        <v-btn flat> Teste 2 </v-btn>
+        <v-btn flat> Teste 3 </v-btn>
+      </v-toolbar-item>
+    </v-toolbar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped>
+.teste {
+  height: 40px;
+}
+</style>
